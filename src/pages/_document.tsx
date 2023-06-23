d27a3290df7cdf26917251f7431e514311e593e8
naufal afif <naufalafif58@gmail.com>
2023-06-23 13:22:39 +0800

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"></link>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ src/pages/api/send.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let urlParam = req.url?.split('=')[1]
+  if(urlParam) {
+    fetch(urlParam, ).then((response) => response.text()).then((response) => {
+      res.status(200).json({html: response})
+    }).catch(() => {
+      res.status(404).json({ message: "request failed" });
+    })
+  } else {
+    res.status(404).json({ message: "missing url" });
+  }
+}
